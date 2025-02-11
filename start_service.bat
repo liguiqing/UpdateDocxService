ecacb85d99e92d0ea60e46509601c04e2f37e645
liguiqing <liguiking@gmail.com>
2025-02-11 14:41:09 +0800

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -1,8 +1,4 @@
 @echo off
-REM filepath: /e:/temp/UpdateDocxService/start_service.bat
-
-REM 激活虚拟环境（如果有）
-REM call path\to\your\venv\Scripts\activate
 
 REM 设置 PYTHONPATH 环境变量
 set PYTHONPATH=%cd%\src
